c5a8bf3cc1ea9cbce68e94fd9f44924eba175971
Abel Akponine <60473441+abelakponine@users.noreply.github.com>
2020-08-09 07:54:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 namespace home;
 use \websocket\ZuppyWS;
 require('websocket/class_loader.php');
-	$host = "34.66.202.57";
+	$host = "127.0.0.1";
 	$port = 2022;
 	// start socket server
-	$zws = new ZuppyWS($host, $port,0,30000);
+	$zws = new ZuppyWS($host, $port,30000,0);
 	$zws::createSocket();
 ?>
